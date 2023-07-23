b34a037a052ccc3e88414f7ac017138e4f0a5a55
CharleneNyabuta <charlenenyabuta@gmail.com>
2023-07-23 22:55:18 +0200

    Created id's for city and weather elements and called showTemperature function

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+function showTemperature(response) {
+  console.log(response.data);
+}
 let apiKey = "e7c95230e9f6b91dafc8b630c003d198";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=paris&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
